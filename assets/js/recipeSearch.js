c756790490e8d4fbe117c221c83ab1cf03f5ea83
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-01-19 12:28:40 +0000

    display recipe names from search

--- assets/js/recipeSearch.js
+++ assets/js/recipeSearch.js
@@ -1,49 +1,80 @@
 const RECIPE_SEARCH_API_ID = "3074c0c2";
 const RECIPE_SEARCH_API_KEY = "c3d552607ffb94d88d65387ada3819bb";
 
-let keyword = "chicken"
-
-let recipeSearchURL = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${RECIPE_SEARCH_API_ID}&app_key=${RECIPE_SEARCH_API_KEY}&q=${keyword}`;
-
-
 // Array of common ingredients for populating the buttons
+const commonIngredients = [
+  "chicken",
+  "pork",
+  "beef",
+  "turkey",
+  "fish",
+  "carrot",
+  "apple",
+  "potato",
+];
 
 // Array of favourite recipe IDs taken from localStorage
+const favouriteRecipies =
+  JSON.parse(localStorage.getItem("recipeSearch_favouriteRecipes")) || [];
 
 // Populate the common ingredients button from the array of ingredients
+function renderCommonIngredients() {
+  for (i = 0; i < commonIngredients.length; i++) {
+    console.log(i);
+  }
+}
 
-// Function to populate the ingredients buttons to the array to search with (in tag)
+// Function to fetch given recipes using search
 
-// Function to fetch a given recipes using search
+// Function to display a single recipe
 
-// Event listener on the recipe search form (submit) to initiate a search
+// Event listener on ingredient form
+$("#addIngredient").on("submit", function (e) {
+  e.preventDefault();
+  console.log("Add Ingredients");
+});
+
+// Event listener on the recipe search button
+$("#searchRecipes").on("click", function () {
+  console.log("Recipe Search");
+
+  fetchRecipes().then((data) => {
+    console.log(data);
+
+    // Array of returned recipes
+    const recipes = data.hits;
+
+    for (i = 0; i < recipes.length; i++) {
+      const recipeHeading = $("<p>").text(recipes[i].recipe.label);
+      $("#recipe-results").append(recipeHeading);
+    }
+  });
+});
 
 // Event listener on recipe favourite button to add to favourites and localStorage
 
+// API search
 async function fetchRecipes() {
-    try {
-      // await response call
-      console.log("Requesting:", recipeSearchURL); 
-      let response = await fetch(recipeSearchURL);
-      console.log("Response Status:", response.status);
+  try {
+    // Get search terms from list use "data-ingredient"
 
-      // once response retrieved, convert to json format
-      let data = await response.json();
+    const tags = "egg+bacon";
 
-      console.log(data);
+    // Construct search URL
+    const recipeSearchURL = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${RECIPE_SEARCH_API_ID}&app_key=${RECIPE_SEARCH_API_KEY}&tag=${tags}`;
 
-      // return data
-      return data;
-    } catch (error) {
-      // console error message
-      console.error("Fetch error:", error)
-    }
-};
+    // await response call
+    console.log("Requesting:", recipeSearchURL);
+    let response = await fetch(recipeSearchURL);
+    console.log("Response Status:", response.status);
 
+    // once response retrieved, convert to json format
+    let data = await response.json();
 
-fetchRecipes().then(data => {
-  console.log(data);
-  // assign data variables here (outputs we need)
-  // filter data to data variables
-});
-
+    // return data
+    return data;
+  } catch (error) {
+    // console error message
+    console.error("Fetch error:", error);
+  }
+}
