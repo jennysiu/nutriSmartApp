988290b456c1ae2c4ec60c651187af6d4f5eccc2
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-01-19 16:08:45 +0000

    make recipe results display as horizonal blocks (in progress)

--- assets/js/recipeSearch.js
+++ assets/js/recipeSearch.js
@@ -39,34 +39,39 @@ $("#searchRecipes").on("click", function () {
   console.log("Recipe Search");
 
   fetchRecipes().then((data) => {
-    console.log(data);
-
     // Array of returned recipes
     const recipes = data.hits;
 
-    for (i = 0; i < recipes.length; i++) {
+    for (let i = 0; i < recipes.length; i++) {
       const recipe = recipes[i].recipe;
-      const recipeHeading = $("<h4>").text(recipe.label);
-      const recipeImg = $("<img>")
-        .attr("src", recipe.images.REGULAR.url)
-        .attr("width", recipe.images.REGULAR.width)
-        .attr("height", recipe.images.REGULAR.height)
-        .attr("alt", recipe.label);
-      /*      
-        const recipeCard = $(`
-        <div class="card text-white" style="background: url('${recipe.images.REGULAR.url}') center/cover no-repeat;">
-          <!-- <div class="card-header">${recipe.label}</div> -->
-          <div class="card-body">
-            <h5 class="card-title">
-              <div class="card-header">${recipe.label}</div> -->
-            </h5>
-            <p class="card-text">${recipe.label}</p>
-          </div>
-        </div>
-      `);
-*/
+      const recipeUri = recipe.uri;
+      const recipeImageWidth = recipe.images.REGULAR.width;
+      const recipeYield = recipe.yield;
 
-      $("#recipe-results").append(recipeHeading, recipeImg);
+      console.log(recipe.ingredients);
+      console.log(recipe.ingredients[0]);
+      console.log(recipe.ingredients[0].food);
+
+      // Get ingredients list
+      const recipeIngredientsList = $("<ul>");
+      for (let j = 0; j < recipe.ingredients.length; j++) {
+        const recipeIngredient = $(`<li>${recipe.ingredients[j].food}</li>`);
+        console.log(`Recipe Ingredient: ${recipe.ingredients[j].food}`);
+        recipeIngredientsList.text(recipeIngredient);
+      }
+
+      const recipeResult = $(`
+
+        <div class="recipeResult" style="cursor:pointer" data-uri="${recipeUri}">
+          <img src="${recipe.images.REGULAR.url}" style="width:${recipeImageWidth}px;height:auto">
+          <h4>${recipe.label}</h4>
+          <div>Ingredients: ${recipeIngredientsList}</div>
+          <a href="#" class="btn btn-secondary btn-sm">Serves ${recipeYield}</a>
+        </div>
+
+      `);
+
+      $("#recipe-results").append(recipeResult);
     }
   });
 });
@@ -84,12 +89,20 @@ async function fetchRecipes() {
     const recipeSearchURL = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${RECIPE_SEARCH_API_ID}&app_key=${RECIPE_SEARCH_API_KEY}&tag=${tags}`;
 
     // await response call
-    console.log("Requesting:", recipeSearchURL);
+    //console.log("Requesting:", recipeSearchURL);
     let response = await fetch(recipeSearchURL);
-    console.log("Response Status:", response.status);
+    //console.log("Response Status:", response.status);
 
     // once response retrieved, convert to json format
     let data = await response.json();
+
+    // Convert the JSON data to a string
+    const jsonString = JSON.stringify(data);
+
+    // Check the length of the string in bytes
+    const byteSize = new Blob([jsonString]).size;
+
+    //console.log(`JSON Data Size: ${byteSize} bytes`);
 
     // return data
     return data;
