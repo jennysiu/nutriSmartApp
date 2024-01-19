cd4212ab9d5e3c4bb607b62f8d07c8fd0caa8284
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-01-19 20:16:32 +0000

    add nav handler pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Target Nav Buttons
+// recipeNavigation
+// nutritionalNavigation
+
+// Event listener for click id recipeNavigation and hide .nutritional-analysis-section
+
+// Event listener for click id nutritionalNavigation and hide .recipe-search-section
+
+// Hide .recipe-search-section on load
