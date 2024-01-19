064b2b6ed81944acd17b24c11830a949158d7646
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-01-19 20:27:00 +0000

    show/hide recipe search and nutrition analysis sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,18 @@
 // nutritionalNavigation
 
 // Event listener for click id recipeNavigation and hide .nutritional-analysis-section
+$("#recipeNavigation").on("click", function () {
+  $(".recipe-search-section").removeClass("d-none");
+  $(".nutritional-analysis-section").addClass("d-none");
+});
 
 // Event listener for click id nutritionalNavigation and hide .recipe-search-section
+$("#nutritionalNavigation").on("click", function () {
+  $(".recipe-search-section").addClass("d-none");
+  $(".nutritional-analysis-section").removeClass("d-none");
+});
 
-// Hide .recipe-search-section on load
+// Hide .nutritional-analysis-section on load
+$(function () {
+  $(".nutritional-analysis-section").addClass("d-none");
+});
