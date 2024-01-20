2798435edfe7070c23b412976198887b43255abd
jennysiu <jenny.siu79@gmail.com>
2024-01-20 21:03:35 +0000

    Added d-none to nutritional analysis container and corrected ids in script js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,14 @@
 $("#recipeNavigation").on("click", function () {
   $(".recipe-search-section").removeClass("d-none");
   $(".randomRecipes").removeClass("d-none");
-  $("#nutritional-analysis-containe").addClass("d-none");
+  $("#nutritional-analysis-container").addClass("d-none");
 });
 
 // Event listener for click id nutritionalNavigation and hide .recipe-search-section
 $("#nutritionalNavigation").on("click", function () {
   $(".recipe-search-section").addClass("d-none");
   $(".randomRecipes").addClass("d-none");
-  $("#nutritional-analysis-containe").removeClass("d-none");
-});
-
-// Hide .nutritional-analysis-section on load
-$(function () {
-  $("#nutritional-analysis-containe").addClass("d-none");
+  $("#nutritional-analysis-container").removeClass("d-none");
 });
 
 // todo: hide nutritional-info section & show when analyse is clicked
