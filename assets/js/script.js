90a7cff51cdfcd6888916b1d8a3cfe7cbfa76b51
jennysiu <jenny.siu79@gmail.com>
2024-01-20 20:51:43 +0000

    Corrected nutritional section reference in script js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,19 @@
 $("#recipeNavigation").on("click", function () {
   $(".recipe-search-section").removeClass("d-none");
   $(".randomRecipes").removeClass("d-none");
-  $("#nutritional-analysis").addClass("d-none");
+  $("#nutritional-analysis-containe").addClass("d-none");
 });
 
 // Event listener for click id nutritionalNavigation and hide .recipe-search-section
 $("#nutritionalNavigation").on("click", function () {
   $(".recipe-search-section").addClass("d-none");
   $(".randomRecipes").addClass("d-none");
-  $("#nutritional-analysis").removeClass("d-none");
+  $("#nutritional-analysis-containe").removeClass("d-none");
 });
 
 // Hide .nutritional-analysis-section on load
 $(function () {
-  $(".nutritional-analysis-section").addClass("d-none");
+  $("#nutritional-analysis-containe").addClass("d-none");
 });
 
 // todo: hide nutritional-info section & show when analyse is clicked
