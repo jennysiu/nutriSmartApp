82b11f351b4ab842096ace086e8ad3924fc1c757
jennysiu <jenny.siu79@gmail.com>
2024-01-21 16:47:35 +0000

    user input error handling

--- assets/js/nutionalAnalysis.js
+++ assets/js/nutionalAnalysis.js
@@ -1,44 +1,62 @@
-// ! todo: find sugar
-// ! todo: add units to nutrients
-// ! todo: add vitamins
-// ! todo: add search logic
+
 // todo: formating for nutritional information
-// ! todo: app name
 // todo: add contact links
-// todo: add error handling for user input
+// todo: user input error handle works but console log will still throw an error
 // todo: local storage - save favourite recipes
-//  todo: filter out some health lables
 // todo: hide nutritional info before search 
 
+// assign global variables
 const NUTRITIONAL_API_ID = "9a70d71a";
 const  NUTRITIONAL_API_KEY = "7fc8d04c2f9d72df3a6d50790e97e17e";
 
 const nutriCardNutrients = ["FAT", "FASAT", "CHOLE", "NA", "CHOCDF", "FIBTG", "SIGAR", "PROCNT"];
 const allVitAndMinerals = ["CA", "FE", "K", "MG", "NIA", "P", "RIBF", "THIA", "TOCPHA", "VITA_RAE", "VITB6A", "VITB12", "VITC", "VITD", "VITK1", "ZN"];
 
+function handleUserInputError(responseCode) {
+    if (responseCode === 555) {
+    $("#custom-search-input-error").text(` We cannot calculate the nutrition for some ingredients. Please check the ingredient spelling or if you have entered a quantities for the ingredients.`)
+    } else {
+      $("#custom-search-input-error").text(`test`)
+    }
+}
+
 // wait for page to fullly load & event listener for analyse button
-document.addEventListener('DOMContentLoaded', (event) => {
+$(document).ready(function() {
   $('#custom-analysis-button').click(function(event) {
       event.preventDefault(); // This will prevent the default action of the button
-      customAnalysisButton();
+      captureUserInput()
+
   });
 });
 
+function captureUserInput() {
+  userIngridients = $(".search-input").val();
+  
+    // fetchNutriInfo to see if repsonse is valid
+    fetchNutritionalInfo(userIngridients).then(response => {
+      if (response.success) {
+        customAnalysis(userIngridients)
+      } else {
+        handleUserInputError(response.errorCode)
+      }
+    });
+  // 
+}
+
+ //  customAnalysis(userIngridients);
 async function fetchNutritionalInfo(userIngridients) {
   try {
+    
     // Define the body of the request
     const nutritionalParameters = {
-      // title: "Chicken Paprikash", // Example title
-      ingr: [userIngridients], // Example ingredients
-      // yield: "string"
-      // Additional fields can be added here as needed
+      ingr: [userIngridients], 
     };
 
     // Define the request URL
     const nutritionalURL = `https://api.edamam.com/api/nutrition-details?app_id=${NUTRITIONAL_API_ID}&app_key=${NUTRITIONAL_API_KEY}`;
 
     // Make a POST request
-    let response = await fetch(nutritionalURL, {
+    const response = await fetch(nutritionalURL, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -46,20 +64,23 @@ async function fetchNutritionalInfo(userIngridients) {
       body: JSON.stringify(nutritionalParameters)
     });
 
-    // Convert response to JSON
     let data = await response.json();
-    // console.log(data);
 
-    // Return the data
-    return data;
+    if (response.status === 200) {
+      return { success: true, data};
+    } else {
+      return { success: false, errorCode: response.status };
+    }
+
   } catch (error) {
     console.error("Fetch error:", error);
+    return { success: false, errorCode: response.status, errorMessage: error.message };
   }
 };
 
 // fetchNutritionalInfo();
 
-function customAnalysisButton() {
+function customAnalysis(userIngridients) {
 
   // clear existing info 
   $(".diet-labels").empty();
@@ -67,7 +88,8 @@ function customAnalysisButton() {
   $("#vit-and-minerals-body").empty();
 
 
-  let userIngridients = $(".search-input").val();
+  // let userIngridients = $(".search-input").val();
+
   // let userIngridients = "1 chicken" 
 
   // fetch & render info
@@ -88,16 +110,14 @@ function customAnalysisButton() {
     }
 
     // health labels
-    // todo: needs to be filtered out
+
     // health labels to remove from list API list
     let unwantedHealthLabels = ["SULPHITE_FREE","SESAME_FREE","SUGAR_CONSCIOUS","SPECIFIC_CARBS","MILK_FREE","FISH_FREE","WHEAT_FREE","MEDITERRANEAN", "DASH", "EGG_FREE","RED_MEAT_FREE","CELERY_FREE","MUSTARD_FREE","LUPINE_FREE","ALCOHOL_FREE","NO_OIL_ADDED","NO_SUGAR_ADDED","FODMAP_FREE" ];
 
     let healthLabels = data.healthLabels;
-    console.log(healthLabels)
+    // new array with unwanted health lables filtered out
     let healthLabelsToKeep = healthLabels.filter(item => !unwantedHealthLabels.includes(item));
     console.log(healthLabelsToKeep)
-
-    console.log(healthLabels);
 
     for (let i = 0; i < healthLabelsToKeep.length; i++) {
       const healthBadge = $("<span>")
@@ -108,8 +128,11 @@ function customAnalysisButton() {
 
     // NURTRITION CARD
     // calories
+    // calories
     let calories = data.calories;
     console.log(calories);
+  
+  $("#total-calories").text(calories);
 
     // *** gives you in quantity and units
     let totalNutrients = data.totalNutrients;
@@ -197,35 +220,13 @@ function customAnalysisButton() {
   });
 }
 
-$(document).ready(function organiseData() {
-  // Get data from local storage or empty array if none exist
-  let data = JSON.parse(localStorage.getItem('savedData')) || [];
+// $(document).ready(function organiseData() {
+//   let data = JSON.parse(localStorage.getItem('savedData'));
+//   console.log(data);
+//   let totalDailyPercentage = data.totalDaily;
+//   console.log(totalDailyPercentage);
+//   // console.log(totalDailyPercentage);
+//   // console.log(Object.keys(totalDailyPercentage))
+  
+// });
 
-  // If localStorage has data then...
-  if (data.length) {
-    console.log(data);
-    let totalDailyPercentage = data.totalDaily;
-    console.log(totalDailyPercentage);
-    // console.log(totalDailyPercentage);
-    // console.log(Object.keys(totalDailyPercentage))
-  
-  // health labels
-    // todo: needs to be filtered out
-    // health labels to remove from list API list
-    let unwantedHealthLabels = ["SULPHITE_FREE","SESAME_FREE","SUGAR_CONSCIOUS","SPECIFIC_CARBS","MILK_FREE","FISH_FREE","WHEAT_FREE","MEDITERRANEAN", "DASH", "EGG_FREE","RED_MEAT_FREE","CELERY_FREE","MUSTARD_FREE","LUPINE_FREE","ALCOHOL_FREE","NO_OIL_ADDED","NO_SUGAR_ADDED","FODMAP_FREE" ];
-  
-    let healthLabels = data.healthLabels;
-    console.log(healthLabels)
-    let healthLabelsToKeep = healthLabels.filter(item => !unwantedHealthLabels.includes(item));
-    console.log(healthLabelsToKeep)
-  
-    console.log(healthLabels);
-  
-    for (let i = 0; i < healthLabelsToKeep.length; i++) {
-      const healthBadge = $("<span>")
-      .addClass("badge label-badge")
-      .text(healthLabelsToKeep[i]);
-      $(".health-labels").append(healthBadge)
-    }
-  }
-});
