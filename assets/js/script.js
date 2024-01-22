e078ec4cc3569300002c68d0a1de5e7567d54811
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-01-22 22:23:39 +0000

    corrected bootstrap footer; removed random recipes; fixed html validation errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 $("#recipeNavigation").on("click", function () {
   $("#recipe-search-section").removeClass("d-none");
   $("#recipe-results-section").removeClass("d-none");
-  // $("#random-recipes-section").removeClass("d-none"); // Disabled for now
   $("#nutritional-analysis-container").addClass("d-none");
   $("#display-user-ingredients").addClass("d-none");
   $("#nutritional-info").addClass("d-none");
@@ -12,6 +11,5 @@ $("#recipeNavigation").on("click", function () {
 $("#nutritionalNavigation").on("click", function () {
   $("#recipe-search-section").addClass("d-none");
   $("#recipe-results-section").addClass("d-none");
-  $("#random-recipes-section").addClass("d-none");
   $("#nutritional-analysis-container").removeClass("d-none");
 });
