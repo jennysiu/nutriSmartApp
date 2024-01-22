85065b5161a1d56172b9d19996dabed0e5e1e905
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-01-22 10:23:17 +0000

    hide nutrition data when nagigating to recipe page

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ $("#recipeNavigation").on("click", function () {
   $("#recipe-results-section").removeClass("d-none");
   // $("#random-recipes-section").removeClass("d-none"); // Disabled for now
   $("#nutritional-analysis-container").addClass("d-none");
+  $("#display-user-ingredients").addClass("d-none");
+  $("#nutritional-info").addClass("d-none");
 });
 
 // Event listener on nav link to show Nutritional Sections
@@ -13,4 +15,3 @@ $("#nutritionalNavigation").on("click", function () {
   $("#random-recipes-section").addClass("d-none");
   $("#nutritional-analysis-container").removeClass("d-none");
 });
-
