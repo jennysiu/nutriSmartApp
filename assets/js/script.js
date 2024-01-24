535effe8d215d6539423f87d1ed8773f155eb946
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-01-24 07:54:03 +0000

    added event listener to favourites link in nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 $("#recipeNavigation").on("click", function () {
   $("#recipe-search-section").removeClass("d-none");
   $("#recipe-results-section").removeClass("d-none");
+  $("#recipe-favourites-section").addClass("d-none");
   $("#nutritional-analysis-container").addClass("d-none");
   $("#display-user-ingredients").addClass("d-none");
   $("#nutritional-info").addClass("d-none");
@@ -11,5 +12,14 @@ $("#recipeNavigation").on("click", function () {
 $("#nutritionalNavigation").on("click", function () {
   $("#recipe-search-section").addClass("d-none");
   $("#recipe-results-section").addClass("d-none");
+  $("#recipe-favourites-section").addClass("d-none");
   $("#nutritional-analysis-container").removeClass("d-none");
+});
+
+// Event listener on nav link to show Favourite Sections
+$("#favouriteNavigation").on("click", function () {
+  $("#recipe-search-section").addClass("d-none");
+  $("#recipe-results-section").addClass("d-none");
+  $("#recipe-favourites-section").removeClass("d-none");
+  $("#nutritional-analysis-container").addClass("d-none");
 });
